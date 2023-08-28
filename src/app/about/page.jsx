8da47b541cdcf2133b93c1592bef7dd488743228
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-28 12:19:56 -0300

    about me page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+const TestingPage = () => {
+  return (
+    <main className="bg-darkPurple h-screen w-full text-white flex items-center justify-center text-2xl">
+      <section className="bg-purple w-11/12 h-[90%] rounded-xl"></section>
+    </main>
+  )
+}
+export default TestingPage
