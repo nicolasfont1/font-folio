a5f2ee193cb6a5cb5a75000e5f34d6e672ea99cc
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-29 16:06:11 -0300

    fixed component height

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 const AboutPage = () => {
   return (
-    <main className="bg-darkPurple h-screen w-full text-white flex items-center justify-center text-2xl">
+    <main className="bg-darkPurple h-screen-mobile w-full text-white flex items-center justify-center text-2xl">
       <section className="bg-purple w-11/12 h-[90%] rounded-xl"></section>
     </main>
   )
