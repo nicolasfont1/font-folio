5ad8c483ab80dd862f991f5982780e9fa67e58d3
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-29 16:06:22 -0300

    added body component

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 import WorkExperienceHeader from "@/components/experience/workExperienceHeader";
+import WorkExperienceBody from "@/components/experience/workExperienceBody";
 import NavbarExperience from "@/components/experience/navbarExperience";
 
 const ExperiencePage = () => {
 	return (
-		<main className="bg-darkPurple h-screen w-full text-white flex items-center justify-center">
+		<main className="bg-darkPurple h-screen-mobile w-full text-white flex items-center justify-center">
       <section className="bg-purple w-11/12 h-[95%] rounded-xl flex flex-col justify-evenly items-center md:w-10/12">
         <WorkExperienceHeader />
+        <WorkExperienceBody />
         <NavbarExperience />
       </section>
 		</main>
