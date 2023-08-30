f72fb9c019a9dc292416a4eef67705e1146d3dea
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-30 20:38:54 -0300

    fixed text size for md screens

--- src/components/experience/workExperienceHeader.jsx
+++ src/components/experience/workExperienceHeader.jsx
@@ -1,6 +1,6 @@
 const WorkExperienceHeader = () => {
   return (
-    <div className="bg-darkPurple w-11/12 h-16 items-center rounded-lg text-center text-5xl py-1">
+    <div className="bg-darkPurple w-11/12 h-16 items-center rounded-lg text-center text-5xl py-1 md:text-6xl">
       Work Experience
     </div>
   )
