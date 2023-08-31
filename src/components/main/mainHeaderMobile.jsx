00e605847451228b13b89ba72d751a0cbdc5fb82
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-31 16:42:35 -0300

    main header mobile done

---
+++ src/components/main/mainHeaderMobile.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+
+const MainHeaderMobile = () => {
+  return (
+    <article className="bg-darkPurple/50 w-11/12 h-32 rounded-full flex flex-row">
+      <div className="bg-darkPurple w-32 h-full rounded-full relative">
+        <Image
+          src={"/various/Me.png"}
+          width={80}
+          height={80}
+          alt="LiterallyMe"
+          className="absolute left-6 top-4"
+        />
+      </div>
+      <div className="flex flex-col h-full justify-center ml-3">
+        <span className="text-3xl h-6 flex items-center ml-1">Nicolás</span>
+        <span className="text-7xl h-12 flex items-center">Font</span>
+        <span className="text-2xl h-6 flex items-center ml-1 mt-1 text-lightBlue">Full Stack Dev</span>
+      </div>
+    </article>
+  )
+}
+export default MainHeaderMobile
