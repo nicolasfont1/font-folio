00fc2a3ea08a5d26a4f267b93a2f592391d1e5c4
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-31 16:09:59 -0300

    starting the main header

--- src/components/main/mainHeader.jsx
+++ src/components/main/mainHeader.jsx
@@ -1,8 +1,15 @@
+import Image from "next/image"
+
 const MainHeader = () => {
   return (
     <article className="bg-darkPurple/50 w-11/12 h-32 rounded-full flex flex-row">
       <div className="bg-darkPurple w-32 h-full rounded-full">
-        Hola
+        <Image
+          src={"/various/Me.png"}
+          width={50}
+          height={50}
+          alt="LiterallyMe"
+        />
       </div>
     </article>
   )
