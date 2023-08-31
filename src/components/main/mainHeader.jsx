ee3c4a5dbc8980f26637cb9a756bdc31fa0e6a78
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-31 16:09:53 -0300

    starting the main header

---
+++ src/components/main/mainHeader.jsx
@@ -0,0 +1,10 @@
+const MainHeader = () => {
+  return (
+    <article className="bg-darkPurple/50 w-11/12 h-32 rounded-full flex flex-row">
+      <div className="bg-darkPurple w-32 h-full rounded-full">
+        Hola
+      </div>
+    </article>
+  )
+}
+export default MainHeader
