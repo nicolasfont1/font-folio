19034187165a7c5a135688bd38feacd781d0408f
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-31 16:42:44 -0300

    changed main header name

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,10 +1,10 @@
-import MainHeader from "@/components/main/mainHeader"
+import MainHeaderMobile from "@/components/main/mainHeaderMobile"
 
 const MainPage = () => {
   return (
     <main className="bg-darkPurple h-screen-mobile w-full text-white flex items-center justify-center">
       <section className="bg-purple w-11/12 h-[95%] rounded-xl flex flex-col justify-evenly items-center md:w-10/12">
-        <MainHeader />
+        <MainHeaderMobile />
       </section>
     </main>
   )
