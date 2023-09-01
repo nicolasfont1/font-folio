5cd88534977a2a154d5aed5e14c04f66b5c17b8a
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-01 00:20:13 -0300

    added components for main view

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,10 +1,14 @@
-import MainHeaderMobile from "@/components/main/mainHeaderMobile"
+import MainHeaderMobile from "@/components/main/mobile/mainHeaderMobile"
+import MainHeaderBubbleMobile from "@/components/main/mobile/mainHeaderBubbleMobile"
+import MainUserBubbleMobile from "@/components/main/mobile/mainUserBubbleMobile"
 
 const MainPage = () => {
   return (
     <main className="bg-darkPurple h-screen-mobile w-full text-white flex items-center justify-center">
       <section className="bg-purple w-11/12 h-[95%] rounded-xl flex flex-col justify-evenly items-center md:w-10/12">
         <MainHeaderMobile />
+        <MainHeaderBubbleMobile />
+        <MainUserBubbleMobile />
       </section>
     </main>
   )
