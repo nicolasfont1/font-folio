17eac61f418382b5fee0d020c136bd30338b6db3
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-01 17:28:02 -0300

    selected option arrow

---
+++ src/components/main/optionSelectedArrow.jsx
@@ -0,0 +1,8 @@
+const OptionSelectedArrow = () => {
+	return (
+		<div className="w-6 max-h-[48px] overflow-visible relative flex items-center justify-center">
+			<span className="text-4xl overflow-visible relative right-1 h-8 w-3 flex items-center shake">{">"}</span>
+		</div>
+	);
+};
+export default OptionSelectedArrow;
