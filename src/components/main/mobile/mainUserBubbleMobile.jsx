707e0b30864afb3cfc9815ea70444480827144ff
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-01 00:19:34 -0300

    user bubble for mobile done

---
+++ src/components/main/mobile/mainUserBubbleMobile.jsx
@@ -0,0 +1,8 @@
+const MainUserBubbleMobile = () => {
+  return (
+    <article className="w-5/6 h-60 bg-white/80 border-black border-8 border-double mb-2">
+      User bubble
+    </article>
+  )
+}
+export default MainUserBubbleMobile
