dc94e2b0e1f2003ffeac77525657c3b09f5a62ca
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-01 17:28:21 -0300

    user bubble done

--- src/components/main/mobile/mainUserBubbleMobile.jsx
+++ src/components/main/mobile/mainUserBubbleMobile.jsx
@@ -1,8 +1,79 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import OptionSelectedArrow from "../optionSelectedArrow";
+
 const MainUserBubbleMobile = () => {
-  return (
-    <article className="w-5/6 h-60 bg-white/80 border-black border-8 border-double mb-2">
-      User bubble
-    </article>
-  )
-}
-export default MainUserBubbleMobile
+	const router = useRouter();
+
+	const [paths, setPaths] = useState({
+		experience: false,
+		projects: false,
+		about: false,
+		play: false,
+	});
+
+	const handlePaths = (path) => {
+		switch (path) {
+      case "experience":
+        if (paths.experience) {
+          return router.push("/experience")
+        } else {
+          return setPaths({ experience: true, projects: false, about: false, play: false });
+        }
+			case "projects":
+        if (paths.projects) {
+          return router.push("/experience")
+        } else {
+          return setPaths({ experience: false, projects: true, about: false, play: false });
+        }
+
+			case "about":
+        if (paths.about) {
+          return router.push("/experience")
+        } else {
+          return setPaths({ experience: false, projects: false, about: true, play: false });
+        }
+
+			case "play":
+        if (paths.play) {
+          return router.push("/experience")
+        } else {
+          return setPaths({ experience: false, projects: false, about: false, play: true });
+        }
+
+			default:
+				return setPaths({ experience: false, projects: false, about: false, play: false })
+		}
+	};
+
+	return (
+		<article className="w-5/6 h-[200px] bg-white/80 border-black border-8 border-double mb-2 text-black text-2xl" tabIndex={0} onBlur={() => handlePaths()}>
+			<div
+				className={`w-full min-h-[32px] h-fit flex justify-end mb-2 text-left ${paths.experience ? "" : "opacity-40"}`}
+				onClick={() => handlePaths("experience")}>
+				{paths.experience && <OptionSelectedArrow />}
+				<p className="w-[255px]">Let me see your <span className="text-lightPurple">experience</span></p>
+			</div>
+			<div
+				className={`w-full min-h-[32px] h-fit flex justify-end mb-2 text-left ${paths.projects ? "" : "opacity-40"}`}
+				onClick={() => handlePaths("projects")}>
+				{paths.projects && <OptionSelectedArrow />}
+				<p className="w-[255px] leading-none">Show me what <span className="text-lightPurple">projects</span> you've got!</p>
+			</div>
+			<div
+				className={`w-full min-h-[32px] h-fit flex justify-end mb-2 text-left ${paths.about ? "" : "opacity-40"}`}
+				onClick={() => handlePaths("about")}>
+				{paths.about && <OptionSelectedArrow />}
+				<p className="w-[255px] leading-none">Tell me more <span className="text-lightPurple">about</span> yourself</p>
+			</div>
+			<div
+				className={`w-full min-h-[32px] h-fit flex justify-end mb-2 text-left ${paths.play ? "" : "opacity-40"}`}
+				onClick={() => handlePaths("play")}>
+				{paths.play && <OptionSelectedArrow />}
+				<p className="w-[255px]">I want to play a <span className="text-lightPurple">game</span></p>
+			</div>
+		</article>
+	);
+};
+export default MainUserBubbleMobile;
