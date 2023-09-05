625edb45167ee2a3aecd14c8659030f979b6df9e
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-05 19:36:05 -0300

    helper for desktop done

--- src/components/main/desktop/helperDesktop.jsx
+++ src/components/main/desktop/helperDesktop.jsx
@@ -1,17 +1,21 @@
+"use client"
 import Image from "next/image";
+import usePressedArrowKeys from "@/hooks/usePressedArrowKeys";
 
 const HelperDesktop = () => {
+  const {pressLeftArrow, pressUpArrow, pressDownArrow, pressRightArrow, pressEnterKey} = usePressedArrowKeys()
+
 	return (
-		<article className="bg-darkPurple w-11/12 h-12 flex flex-row justify-center items-center opacity-50 sm:text-xl xl:text-3xl mr-2">
+		<article className="no-drag bg-darkPurple w-11/12 h-12 flex flex-row justify-center items-center opacity-50 sm:text-xl xl:text-3xl mr-2">
       <span className="mr-2">Psst! Use</span>
 			<div className="flex flex-row gap-2 mr-2">
-        <Image src={"/navbar/ArrowLeft.png"} width={35} height={35} alt="ArrowLeft" />
-        <Image src={"/navbar/ArrowUp.png"} width={35} height={35} alt="ArrowUp" />
-        <Image src={"/navbar/ArrowDown.png"} width={35} height={35} alt="ArrowDown" />
-        <Image src={"/navbar/ArrowRight.png"} width={35} height={35} alt="ArrowRight" />
+        <Image src={"/navbar/ArrowLeft.png"} width={35} height={35} alt="ArrowLeft" className={`no-drag ${pressLeftArrow}`} />
+        <Image src={"/navbar/ArrowUp.png"} width={35} height={35} alt="ArrowUp" className={`no-drag ${pressUpArrow}`} />
+        <Image src={"/navbar/ArrowDown.png"} width={35} height={35} alt="ArrowDown" className={`no-drag ${pressDownArrow}`} />
+        <Image src={"/navbar/ArrowRight.png"} width={35} height={35} alt="ArrowRight" className={`no-drag ${pressRightArrow}`} />
       </div>
       <span className="mr-2">to move between the menus, and</span>
-      <Image src={"/navbar/EnterKey.png"} width={80} height={35} alt="EnterKey" className="mr-2"/>
+      <Image src={"/navbar/EnterKey.png"} width={80} height={35} alt="EnterKey" className={`no-drag mr-2 ${pressEnterKey}`}/>
       <span>to select.</span>
 		</article>
 	);
