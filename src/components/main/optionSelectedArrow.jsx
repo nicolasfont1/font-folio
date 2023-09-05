f76bd63daf741201d8c9ca6b0dcc518e53fb06ea
nicolasfont1 <nicolasfont15@gmail.com>
2023-09-05 17:10:17 -0300

    fixed spaces

--- src/components/main/optionSelectedArrow.jsx
+++ src/components/main/optionSelectedArrow.jsx
@@ -1,7 +1,7 @@
 const OptionSelectedArrow = () => {
 	return (
-		<div className="w-6 max-h-[48px] overflow-visible relative flex items-center justify-center">
-			<span className="text-4xl overflow-visible relative right-1 h-8 w-3 flex items-center shake">{">"}</span>
+		<div className="w-[9%] max-h-[48px] overflow-visible relative flex items-center justify-center">
+			<span className="text-4xl overflow-visible relative right-[2px] h-8 w-3 flex items-center shake">{">"}</span>
 		</div>
 	);
 };
